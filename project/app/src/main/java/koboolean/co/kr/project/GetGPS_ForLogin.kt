99e852855b3a489e96293241ecda1784f96c8741
DESKTOP-F835TER\user <koboolean@gmail.com>
2018-11-21 19:19:30 +0900

    -Koboolean   2018.11.21   - 로그인 정보 받아와 GPS Activity로 이동 및 id 및 이름 들고오기 완료   - 추후 할일 = GPS 정보 데이터베이스에 넣기

--- project/app/src/main/java/koboolean/co/kr/project/GetGPS_ForLogin.kt
+++ project/app/src/main/java/koboolean/co/kr/project/GetGPS_ForLogin.kt
@@ -3,6 +3,7 @@ package koboolean.co.kr.project
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_get_gps__for_login.*
 import okhttp3.*
 import org.json.JSONArray
@@ -12,10 +13,11 @@ import java.io.IOException
 class GetGPS_ForLogin : AppCompatActivity() {
     var web_id : String? = null
     var web_pw : String? = null
-
+    var activity : Intent? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_gps__for_login)
+        activity = Intent(this, Get_GPSActivity::class.java)
         login_button.setOnClickListener { view ->
             web_id = check_id.text.toString()
             web_pw = check_pw.text.toString()
@@ -47,16 +49,23 @@ class GetGPS_ForLogin : AppCompatActivity() {
     inner class CallBack1 : Callback{
         // 서버와의 통신이 실패되었을 때
         override fun onFailure(call: Call, e: IOException) {
-
+            textView3.text = "현재 통신이 원활하지 않습니다."
         }
         // 서버와의 통신이 잘 마무리 되었을 때
         override fun onResponse(call: Call, response: Response) {
             var result = response?.body()?.string()
 
             runOnUiThread{
-                var obj = JSONObject(result)
-                var name = obj.getString("user_name")
-                textView.text = name
+                    var obj = JSONObject(result)
+                    var name = obj.getString("user_name")
+                if(name.equals("?")){
+                    textView3.text = "아이디 및 비밀번호를 다시 확인해주세요"
+                }else{
+                    activity?.putExtra("name", name)
+                    activity?.putExtra("web_id", web_id)
+                    startActivity(activity)
+                }
+
             }
         }
     }
